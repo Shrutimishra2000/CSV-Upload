201f3023b6f8d7dc8fe98c66eaf2eafb6e35ff9d
Shrutimishra2000 <141424015+Shrutimishra2000@users.noreply.github.com>
2023-08-04 21:38:28 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
    try {
       // console.log("dfsfsfs ",process.env.MONGO_URI)
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect('mongodb+srv://shrutimishra7800:shruti123456@cluster0.lnibqin.mongodb.net/?retryWrites=true&w=majority');
 
       console.log(`                                 MongoDB Connection Established      \n`);
    } catch (error) {
